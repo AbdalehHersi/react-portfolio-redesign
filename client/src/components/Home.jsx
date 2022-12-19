7c5f4a7d2781bb6ba14e4de04c7cd6f556fe7554
Abdaleh Hersi <therealhersi03@gmail.com>
2022-12-19 20:44:40 +0000

    homepage layout

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -10,17 +10,19 @@ export default function Home() {
                 <div className="aboutHeaderContainer">
                     <h1 className="aboutHeader">About Me</h1>
                 </div>
-                <p>I'm an aspiring full-stack web developer with a passion for all things technological.
-                    With a couple of projects under my belt and a bunch of technologies at hand,
-                    I feel ready to take on the world.</p>
-                <p>
-                    I love gaming and art and I love creative developing.
-                    My goal is to combine these two passions into an explosive result
-                    that's breathtakingly fun in the eyes.
-                </p>
-                <cite>
-                    “Setting goals is the first step in turning the invisible into the visible.” — Tony Robbins
-                </cite>
+                <div className="aboutText gradient-border">
+                    <p>I'm an aspiring full-stack web developer with a passion for all things technological.
+                        With a couple of projects under my belt and a bunch of technologies at hand,
+                        I feel ready to take on the world.</p>
+                    <p>
+                        I love gaming and art and I love creative developing.
+                        My goal is to combine these two passions into an explosive result
+                        that's breathtakingly fun in the eyes.
+                    </p>
+                    <cite>
+                        “Setting goals is the first step in turning the invisible into the visible.” — Tony Robbins
+                    </cite>
+                </div>
             </div>
         </div>
     )
