1c90a2775b951a937447452845054793a1364e5d
Abdaleh Hersi <therealhersi03@gmail.com>
2022-12-23 14:20:01 +0000

    added skills section

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -1,19 +1,20 @@
 import CV from "../assets/Abdaleh Hersi - Resume.pdf"
+import "../css/Skills.css"
 
 export default function Skills() {
     return (
         <div className="skillContainer focus-in">
             <a href={CV} alt="CV" download="CV">Download My CV</a>
             <h1>Skills, Strengths and Proficiencies:</h1>
-            <ul>
-                <li>HTML, CSS, Javascript</li>
-                <li>Node.JS</li>
-                <li>MongoDB</li>
-                <li>React</li>
-                <li>Express</li>
-                <li>Experience using and handling APIs</li>
-                <li>SQL</li>
-                <li>MVC Paradigm</li>
+            <ul className="listContainer gradient-border">
+                <li className="listItem">HTML, CSS, Javascript</li>
+                <li className="listItem">Node.JS</li>
+                <li className="listItem">MongoDB</li>
+                <li className="listItem">React</li>
+                <li className="listItem">Express</li>
+                <li className="listItem">Experience using and handling APIs</li>
+                <li className="listItem">SQL</li>
+                <li className="listItem">MVC Paradigm</li>
             </ul>
         </div>
     )
