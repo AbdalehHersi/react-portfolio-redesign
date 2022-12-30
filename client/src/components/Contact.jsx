3c76e61e3912ae355983d68880d99b7ad2ac4755
Abdaleh Hersi <therealhersi03@gmail.com>
2022-12-30 18:39:47 +0000

    changes

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -8,19 +8,19 @@ export default function Contact() {
         <div className="focus-in">
             <address>
                 <div className="contactContainer gradient-border">
-                <h1>You can email me on <a href="mailto:hersiabdaleh@gmail.com" id="link">hersiabdaleh@gmail.com</a>.</h1>
-                <h1>Or you can check out my social media:</h1>
-                <div className="socials">
-                    <a href="https://github.com/AbdalehHersi" target="_blank">
-                        <img src={Github} alt="Github" className="icons" />
-                    </a>
-                    <a href="https://stackoverflow.com/users/19364901/abdaleh-hersi" className="icons" target="_blank">
-                        <img src={StackOverflow} alt="stackoverflow" className="icons" />
-                    </a>
-                    <a href="https://www.linkedin.com/in/abdaleh-hersi-605583242/" className="icons" target="_blank">
-                        <img src={LinkedIn} alt="linkedin" className="icons" />
-                    </a>
-                </div>
+                    <h1>You can email me on <a href="mailto:hersiabdaleh@gmail.com" id="link">hersiabdaleh@gmail.com</a>.</h1>
+                    <h1>Or you can check out my social media:</h1>
+                    <div className="socials">
+                        <a href="https://github.com/AbdalehHersi" target="_blank">
+                            <img src={Github} alt="Github" className="icons" />
+                        </a>
+                        <a href="https://stackoverflow.com/users/19364901/abdaleh-hersi" className="icons" target="_blank">
+                            <img src={StackOverflow} alt="stackoverflow" className="icons" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/abdaleh-hersi-605583242/" className="icons" target="_blank">
+                            <img src={LinkedIn} alt="linkedin" className="icons" />
+                        </a>
+                    </div>
                 </div>
             </address>
         </div>
